2100572fc4323b56131a7cfaa65917010a265d34
Akiva Wolberg <15022566+tshynik@users.noreply.github.com>
2025-05-29 12:41:39 -0400

    translate hex to first 16 alphabet for better windows file sorting

--- ordered_hash.py
+++ ordered_hash.py
@@ -13,8 +13,15 @@ logging.basicConfig(
 	]
 )
 
+
+translation_table = str.maketrans(
+	"0123456789abcdef",
+	"abcdefghijklmnop"
+	)
+
 # list all the files in the Windows folder:
-list_of_images = list(Path('mid').glob('**/*.jpg'))
+list_of_images = list(Path('faves').glob('**/*.jpg'))
+hash_name = 'ds3_'
 
 logging.info(f"found {len(list_of_images)} to process")
 count = 0
@@ -26,18 +33,22 @@ for image in list_of_images:
 	# dhash format: dha_c1c4e4a484a0809083fbffcc0040831e_[stem]
 	#   - using dhash.format_hex(row, col)
 	
-	if image.stem[:4] != "dhs_":
+	if image.stem[:4] != hash_name:
 		with Image.open(image) as image_stream:
 			row, col = dhash.dhash_row_col(image_stream)
 			img_hash_orig = dhash.format_hex(row, col)
 			# decompose into hex digits, then sort and re-encode and combine:
 			img_hash = ''.join([hex(item)[2:4] for item in sorted(bytes.fromhex(img_hash_orig))])
-
+			# print(img_hash)
+			img_hash1 = img_hash.translate(translation_table)
+			# print(img_hash1)
 		# overwrite md5_ hash:
-		if image.stem[:4] == "md5_" or image.stem[:4] == "dha_":
-			image.rename(image.with_stem(f"dhs_{img_hash}_{image.stem[37:]}"))
+		if image.stem[:4] == "md5_" or image.stem[:4] == "dha_" or image.stem[:4] == "dhs_" or image.stem[:4] == "ds2_":
+			image.rename(image.with_stem(f"{hash_name}{img_hash1}_{image.stem[35:]}"))
+			# print( image.with_stem(f"{hash_name}{img_hash1}_{image.stem[35:]}") )
 		else:
-			image.rename(image.with_stem(f"dhs_{img_hash}_{image.stem}"))
+			image.rename(image.with_stem(f"{hash_name}{img_hash1}_{image.stem}"))
+			# print( image.with_stem(f"{hash_name}{img_hash1}_{image.stem}") )
 
 	if count % 25 == 0:
 		logging.info(f"processed {count}")
