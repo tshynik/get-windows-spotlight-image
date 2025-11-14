77b4aa8bf3d32436bd4ffd535f762daad70dbcad
Akiva Wolberg <15022566+tshynik@users.noreply.github.com>
2025-11-14 18:30:24 -0500

    do image hash within get_today_img

--- get_today_img.py
+++ get_today_img.py
@@ -5,10 +5,10 @@ to the script folder, and adds a file type extension for those missing one.
 
 import shutil
 from pathlib import Path
-# from PIL import Image
-# import subprocess
 from datetime import datetime as dt
 import json
+import dhash
+from PIL import Image # pip install pillow
 
 # via https://stackoverflow.com/questions/13733552:
 import logging
@@ -21,8 +21,21 @@ logging.basicConfig(
 		logging.StreamHandler()
 	]
 )
-
 logging.info("NEW RUN")
+
+
+def clean_filename(filestem:str) -> str:
+	filestem = filestem.lower()
+	filestem = filestem.replace(', ','-')
+	filestem = filestem.replace(' ','_')
+	return filestem
+
+## img hash settings
+translation_table = str.maketrans(
+	"0123456789abcdef",
+	"abcdefghijklmnop"
+	)
+hash_name = 'ds3_'
 
 # where to get the files:
 folder_login = Path.home() / Path("AppData/Local/Packages/Microsoft.Windows.ContentDeliveryManager_cw5n1h2txyewy/LocalState/Assets")
@@ -50,8 +63,6 @@ spotlight_files_candidates = list(folder_login.glob('**/*')) + list(folder_deskt
 
 for file in spotlight_files_candidates:
 
-	# print(file.name)
-
 	if file.stat().st_size > 200000:
 		# get file name and modification time:
 		file_mod_time = dt.fromtimestamp(file.stat().st_mtime)
@@ -64,11 +75,22 @@ for file in spotlight_files_candidates:
 
 			# copy the image:
 			new_path = today / Path(file.stem + '.jpg')
-			# print(new_path)
 			shutil.copy( file, new_path )
 			logging.info("copied!")
+			
+			## create hash:
+			with Image.open(new_path) as image_stream:
+				row, col = dhash.dhash_row_col(image_stream)
+				img_hash_orig = dhash.format_hex(row, col)
+				# decompose into hex digits, then sort and re-encode and combine:
+				img_hash = ''.join([hex(item)[2:4] for item in sorted(bytes.fromhex(img_hash_orig))])
+				img_hash1 = img_hash.translate(translation_table)
+			# don't need to check for existing hash in filename because these are new files
+			stem = clean_filename(new_path.stem)
+			new_path.rename(new_path.with_stem(f"{hash_name}{img_hash1}_{stem}"))
+			logging.info(f"renamed to {new_path.stem}")
 		else:
-			logging.info(f"OLD - {file.name} modified {file_mod_time}")
+			logging.info(f"OLD - {file.name[:20]}..., modified {file_mod_time}")
 
 		# if I wanted to do any sorting based on resolution:
 
@@ -81,12 +103,3 @@ log = {'latest_file': new_latest_file_time }
 with open('logs.json', mode = 'w', encoding = 'UTF-8') as log_file:
 	json.dump(log,log_file, default = str)
 logging.info(f"Adjusted datetime cutoff for new files: {new_latest_file_time}")
-
-# open Windows Explorer at the end to review:
-# subprocess.Popen(fr'explorer /select,"{today.resolve()}"') ### doesn't work right anyway....
-
-
-## TODO: checking for duplicates with the same name: MD5 hash?
-# But I think they're already hashed, must be subtle differences. Hm.
-# import hashlib
-# hash = hashlib.MD5(your_text_here).hexdigest()
